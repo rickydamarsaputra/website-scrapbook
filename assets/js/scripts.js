b08e5acd7d5fa9d6849d7c2d01da6e94d7157458
rickydamarsaputra <retrocode.rc@gmail.com>
2022-06-02 18:25:21 +0700

    inisiasi awal turnjs

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+function loadApp() {
+	$('.flipbook').turn({
+		width: 922,
+		height: 600,
+		elevation: 50,
+		gradients: true,
+		autoCenter: true,
+	});
+}
+
+yepnope({
+	test: Modernizr.csstransforms,
+	yep: ['/assets/js/turn.js'],
+	nope: ['/assets/js/turn.html4.min.js'],
+	both: ['/assets/css/basic.css'],
+	complete: loadApp,
+});
+
+$('.flipbook').bind('turned', function (event, page, view) {
+	if (page != 1) {
+		const audio = new Audio('/assets/audio/flip-sound.mp3');
+		audio.play();
+	}
+});
