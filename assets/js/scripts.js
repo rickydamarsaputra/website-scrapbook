3aac5d645082c3e723778202f5621ece8f6e8772
rickydamarsaputra <retrocode.rc@gmail.com>
2022-06-03 21:11:47 +0700

    sementara drawing canvas (mouse mbereset, sama kalau 2 canvas entah kenapa enggak bisa)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -22,8 +22,77 @@ $('.flipbook').bind('turned', function (event, page, view) {
 		audio.play();
 
 		view.forEach(function (val) {
-			gsap.from(`[data-page="${val}"]`, { y: -100, duration: 1 });
-			// console.log(document.querySelector(`[data-page="${val}"]`));
+			gsap.fromTo(`img[data-page="${val}"]`, { opacity: 0, y: -100 }, { opacity: 1, y: 0, duration: 1 });
+
+			// const canvas = document.querySelector(`canvas[data-page="${val}"]`);
+			// const ctx = canvas.getContext('2d');
+			// let painting = false;
+
+			// function startDraw(e) {
+			// 	painting = true;
+			// 	draw(e);
+			// }
+
+			// function endDraw(e) {
+			// 	painting = false;
+			// 	ctx.beginPath();
+			// }
+
+			// function draw(e) {
+			// 	if (!painting) return;
+			// 	const x = e.clientX;
+			// 	const y = e.clientY;
+
+			// 	console.log({ x, y, canvas });
+
+			// 	ctx.linewidth = 20;
+			// 	ctx.lineCap = 'round';
+			// 	ctx.strokeStyle = 'white';
+
+			// 	ctx.lineTo(x, y);
+			// 	ctx.stroke();
+			// 	ctx.beginPath();
+			// 	ctx.moveTo(x, y);
+			// }
+
+			// canvas.addEventListener('mousedown', startDraw);
+			// canvas.addEventListener('mouseup', endDraw);
+			// canvas.addEventListener('mousemove', draw);
 		});
 	}
 });
+
+const canvas = document.querySelector('.drawing');
+
+const ctx = canvas.getContext('2d');
+let painting = false;
+
+function startDraw(e) {
+	painting = true;
+	draw(e);
+}
+
+function endDraw(e) {
+	painting = false;
+	ctx.beginPath();
+}
+
+function draw(e) {
+	if (!painting) return;
+	const x = e.clientX;
+	const y = e.clientY;
+	console.log(x, y);
+
+	ctx.linewidth = 20;
+	ctx.lineCap = 'round';
+	ctx.strokeStyle = 'white';
+
+	ctx.lineTo(x, y);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.moveTo(x, y);
+}
+
+canvas.addEventListener('mousedown', startDraw);
+canvas.addEventListener('mouseup', endDraw);
+canvas.addEventListener('mousemove', draw);
